0a340930f602c55a467a61ace78ef44ef7fa1760
tikhonp <tikhonp@users.noreply.github.com>
2023-08-22 23:14:59 +0300

    🚧 Fix start call

--- utils/voximplant.py
+++ utils/voximplant.py
@@ -3,32 +3,27 @@ import json
 import requests
 from django.conf import settings
 
-VOXIMPLANT_CALLER_ID = settings.VOXIMPLANT_CALLER_ID
-PARAMS = {
-    'access_token': settings.VOXIMPLANT_ACCESS_TOKEN,
-    'domain': settings.VOXIMPLANT_ACCOUNT_NAME
-}
-BASE_URL = f"https://{settings.VOXIMPLANT_API_HOSTNAME}/api/v3"
-HOST = settings.HOST
-
 
 def run_scenario(scenario_id: int, phone: str, call_id: int, agent_token: str) -> bool:
     """Runs a voximplant scenario."""
 
-    url = BASE_URL + "/scenario/runScenario"
-
+    url = f"https://{settings.VOXIMPLANT_API_HOSTNAME}/api/v3/scenario/runScenario"
+    params = {
+        'access_token': settings.VOXIMPLANT_ACCESS_TOKEN,
+        'domain': settings.VOXIMPLANT_ACCOUNT_NAME
+    }
     data = {
         'scenario_id': scenario_id,
         'phone': phone,
         'variables': json.dumps({
             'call_id': call_id,
             'agent_token': agent_token,
-            'host': HOST
+            'host': settings.HOST
         }),
-        'caller_id': VOXIMPLANT_CALLER_ID
+        'phone_number_id': settings.VOXIMPLANT_CALLER_ID
     }
+    answer = requests.post(url, params=params, data=data).json()
 
-    answer = requests.post(url, params=PARAMS, data=data).json()
     result = answer.get('success', False)
     if not result:
         print(f"run_scenario failed: {answer}")
