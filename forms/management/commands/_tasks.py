d0af53ac05f6f4bb055e63854b33afa5fa13717b
tikhonp <tikhonp@users.noreply.github.com>
2023-08-28 13:28:18 +0300

    🚧 fix check_current_calls db query

--- forms/management/commands/_tasks.py
+++ forms/management/commands/_tasks.py
@@ -11,9 +11,11 @@ def get_contracts_with_active_form(time_from: time, time_to: time, date_from: da
     return (
         Contract
         .objects
-        # .select_related('forms')
+        .prefetch_related('forms')
+        .prefetch_related('time_slot_set')
+        .prefetch_related('forms__call_set')
         .exclude(forms__isnull=True)
-        .filter(time_slot_set__time__in=(time_from, time_to))
+        .filter(time_slot_set__time__range=(time_from, time_to))
         .exclude(
             Q(forms__call_set__updated_at__gte=date_from) &
             Q(forms__call_set__state=Call.State.SUCCESS)
@@ -26,10 +28,11 @@ def check_current_calls():
 
     now = datetime.now()
     to_time = (now + timedelta(minutes=1)).time()
+    from_time = now.time()
     from_date = now - timedelta(days=1)
     print(f"now: {now}\nto_time: {to_time}\nfrom_date: {from_date}")
 
-    contracts_with_active_forms = get_contracts_with_active_form(now.time(), to_time, from_date)
+    contracts_with_active_forms = get_contracts_with_active_form(from_time, to_time, from_date)
     print("contracts_with_active_forms: ", contracts_with_active_forms)
 
     for contract in contracts_with_active_forms:
