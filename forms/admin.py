fa2ac9ea1239f1d9c5753bf049f5d12a3c6831a7
tikhonp <tikhon.petrishchev@gmail.com>
2024-01-22 22:56:13 +0300

    :bug: Fixed calls csv export

--- forms/admin.py
+++ forms/admin.py
@@ -16,23 +16,22 @@ class TimeSlotAdmin(admin.ModelAdmin):
     list_display = ('time', 'connected_form')
 
 
-@admin.action(description="Export selected calls for statistic")
-def export_as_csv(self, request, queryset):
-    meta = self.model._meta
-    field_names = ['id', 'created_at', 'updated_at', 'state', 'patient_name', 'patient_phone']
+@admin.action(description="Export selected calls as CSV")
+def export_calls_as_csv(self, request, queryset):
+    field_names = ['id', 'created_at', 'updated_at', 'state', 'patient_name', 'patient_phone', 'is_incoming']
 
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(
-        "voximplant-agent calls"
-    )
-    writer = csv.writer(response)
+    response['Content-Disposition'] = f'attachment; filename={"exported_voximplant_calls"}.csv'
 
+    writer = csv.writer(response)
     writer.writerow(field_names)
+
     for obj in queryset:
-        row = writer.writerow([
+        writer.writerow([
             obj.id, obj.created_at, obj.updated_at, obj.state,
             obj.connected_form.contract.patient_name,
             obj.connected_form.contract.patient_phone,
+            obj.is_incoming
         ])
 
     return response
@@ -41,9 +40,9 @@ def export_as_csv(self, request, queryset):
 @admin.register(models.call.Call)
 class CallAdmin(admin.ModelAdmin):
     list_display = ('id', 'connected_form', 'state', 'created_at')
-    list_filter = ('state',)
+    list_filter = ('created_at', 'state', 'is_incoming')
     readonly_fields = ('created_at', 'updated_at')
-    actions = [export_as_csv]
+    actions = [export_calls_as_csv]
 
 
 @admin.register(models.ConnectedForm)
