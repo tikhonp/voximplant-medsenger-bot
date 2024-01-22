d9b58b4216c7849e523e8f0e1b7cd1d8ab968034
tikhonp <tikhon.petrishchev@gmail.com>
2024-01-22 17:16:04 +0300

    :sparkles: Added calls csv export

--- forms/admin.py
+++ forms/admin.py
@@ -1,4 +1,7 @@
+import csv
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from forms import models
 
@@ -13,11 +16,34 @@ class TimeSlotAdmin(admin.ModelAdmin):
     list_display = ('time', 'connected_form')
 
 
+@admin.action(description="Export selected calls for statistic")
+def export_as_csv(self, request, queryset):
+    meta = self.model._meta
+    field_names = [field.name for field in meta.fields]
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(
+        "voximplant-agent calls"
+    )
+    writer = csv.writer(response)
+
+    writer.writerow(field_names)
+    for obj in queryset:
+        row = writer.writerow([
+            obj.id, obj.created_at, obj.updated_at, obj.state,
+            obj.connected_form.contract.patient_name,
+            obj.connected_form.contract.patient_phone,
+        ])
+
+    return response
+
+
 @admin.register(models.call.Call)
 class CallAdmin(admin.ModelAdmin):
-    list_display = ('id', 'connected_form', 'state', 'created_at',)
+    list_display = ('id', 'connected_form', 'state', 'created_at')
     list_filter = ('state',)
     readonly_fields = ('created_at', 'updated_at')
+    actions = [export_as_csv]
 
 
 @admin.register(models.ConnectedForm)
